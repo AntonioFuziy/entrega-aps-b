3285f36d8e2b9a395a61212b3514e2c740c698d8
AntonioFuziy <49486453+AntonioFuziy@users.noreply.github.com>
2020-06-24 12:57:56 -0400

    adicionando requisito1 e 2

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/APS.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/APS.java
@@ -11,6 +11,7 @@ public class APS {
         model.add(new OrGate());
         model.add(new NotGate());
         model.add(new XorGate());
+        model.add(new NorGate());
 
         View view = new View(model);
 
